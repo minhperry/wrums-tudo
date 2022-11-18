3423f55d555d65b78e245c15a208bf207a27e858
minhperry <46137516+minhperry@users.noreply.github.com>
2022-11-18 21:20:26 +0100

    Add files via upload

---
+++ blatt3/blatt3a3.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+dataX = np.array([9, 6, 12, 10, 8, 9])
+dataY = np.array([130, 125, 145, 140, 130, 140])
+
+plt.scatter(dataX, dataY)
+plt.show()
